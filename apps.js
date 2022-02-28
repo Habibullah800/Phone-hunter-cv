98c0958958a07edc92f13294b27893d003070525
Habibullah800 <mdhabibullah800@gmail.com>
2022-03-01 00:35:30 +0600

    api fetch

--- apps.js
+++ apps.js
@@ -1,1 +1,9 @@
-// alert(2)
+const allMobiles = () => {
+    const searchValue = document.getElementById('search-box').value;
+    const url = `https://openapi.programming-hero.com/api/phones?search=${searchValue};
+    `
+    fetch(url)
+        .then((Response) => Response.json())
+        .then((data) => console.log(data));
+    // console.log(url)
+}
