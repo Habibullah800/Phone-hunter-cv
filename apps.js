3a9e3cff4314567083787d8b52f31d979d82ad59
Habibullah800 <mdhabibullah800@gmail.com>
2022-03-01 23:20:25 +0600

    Api id and details added

--- apps.js
+++ apps.js
@@ -1,9 +1,88 @@
 const allMobiles = () => {
-    const searchValue = document.getElementById('search-box').value;
-    const url = `https://openapi.programming-hero.com/api/phones?search=${searchValue};
-    `
+    const searchfield = document.getElementById('search-box');
+    const searchText = searchfield.value;
+    searchfield.value = ' ';
+    const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`;
+
     fetch(url)
-        .then((Response) => Response.json())
-        .then((data) => console.log(data));
-    // console.log(url)
+        .then(res => res.json())
+        .then(data => showMobileDetails(data.data));
+}
+
+const showMobileDetails = (data) => {
+    const searchResult = document.getElementById('search-result')
+    data.forEach(mobile => {
+
+        const div = document.createElement('div');
+        div.classList.add('col');
+        div.innerHTML = `
+        
+        <div class="card my-4 ">
+               <img class="rounded mx-auto d-block my-3" src="${mobile.image}" height="340px" width="260px" alt="Card image cap">
+            <div class="card-body">
+                <h5 class="card-title text-center ">Brand: ${mobile.brand} </h5>
+                <h4 class="text-center">Model: ${mobile.phone_name}</h4>
+
+                <p class="card-text">If you are interested in this phone then click on Details below for more information, Thank you.</p>
+                
+                <i>
+                <button onclick="details('${mobile.slug}')"   class="btn btn-primary d-flex justify-content-center w-100">
+                Details
+                    </button> </i>
+             </div>
+        </div> `;
+        searchResult.appendChild(div)
+    })
+}
+
+const details = (info) => {
+    const url = `https://openapi.programming-hero.com/api/phone/${info} `;
+    fetch(url)
+        .then(res => res.json())
+        .then(data => setDetails(data.data))
+};
+
+const setDetails = (info) => {
+
+    document.getElementById('details-container').innerHTML = `
+    <div class="card mx-auto" style="width: 60rem; background-color:#f1f2f6">
+    
+    <img class="rounded mx-auto d-block my-3" src="${info.image}" height="400px" width="320px" alt="Card image cap">
+
+    <div class="card-body">
+        <h5 class="card-title text-center">
+        Name of Phone: ${info.name} 
+        
+        </h5>
+        <p class="card-text text-center">
+        Released On: ${info.releaseDate}
+            </p>
+    </div>
+    <div  class="row">
+        <div class="col-6">
+            <ul class="list-group list-group-flush">
+                <li style="background-color:aquamarine" class="list-group-item"> Storage: ${info.mainFeatures.storage} </li>
+                <li style="background-color:azure" class="list-group-item"> Chipset: ${info.mainFeatures.chipSet} </li>
+                <li style="background-color: bisque;" class="list-group-item"> USB:${info.others.USB} </li>
+                <li style="background-color:azure" class="list-group-item"> WLAN: ${info.others.WLAN} </li>
+                <li style="background-color:aquamarine" class="list-group-item"> Radio:${info.others.Radio} </li>
+            </ul>
+        </div>
+        <div class="col-6">
+            <ul class="list-group list-group-flush">
+                <li style="background-color:aquamarine" class="list-group-item"> Display Size: ${info.mainFeatures.displaySize} </li>
+                <li style="background-color:azure" class="list-group-item"> Memory: ${info.mainFeatures.memory} </li>
+                <li style="background-color: bisque;" class="list-group-item"> GPS: ${info.others.GPS} </li>
+                <li style="background-color:azure" class="list-group-item"> Bluetooth: ${info.others.Bluetooth}  </li>
+                <li style="background-color:aquamarine" class="list-group-item"> NFC: ${info.others.NFC} </li>
+            </ul>
+        </div>
+    </div>
+</div>
+
+
+`
+
+    console.log(info.image);
+
 }
